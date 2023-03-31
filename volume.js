6aae03070d15bc03934e810d2bab586cdb85e673
Marcel <mrizzo25@gmail.com>
2023-03-31 04:16:57 -0300

    add API binance

--- volume.js
+++ volume.js
@@ -1,1 +1,8 @@
 import fetch from "node-fetch";
+
+const getVolumeChanges = async () => {
+    const response = await fetch("https://api.binance.com/api/v3/ticker/24hr");
+    const data = await response.json();
+
+    const volumeIncreases = {};
+}  
